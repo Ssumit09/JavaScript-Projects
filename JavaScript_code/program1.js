93e154c1e9fdf6e38e00078b7d4cf3823e1f8723
Sumit Kumar Jha <137000536+Ssumit09@users.noreply.github.com>
2024-09-28 18:09:52 +0000

    adding new Program

---
+++ JavaScript_code/program1.js
@@ -0,0 +1,9 @@
+"use strict"; //Treat all Js code as newer version
+//alert(3+3) // we are using nodejs, not browser so it will not work
+
+let state; // undefined i.e variable created but value doesn't assigned
+//symbol datatype==> for uniqueness
+
+let score="33"
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
