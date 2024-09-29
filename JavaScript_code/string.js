b829fb7f0bce5b2d72c28c9cc297afd81c93f97c
Sumit Kumar Jha <137000536+Ssumit09@users.noreply.github.com>
2024-09-29 12:10:49 +0000

    Added string js file

---
+++ JavaScript_code/string.js
@@ -0,0 +1,6 @@
+const name="hello"
+console.log("hello"+name);  //This is old one
+console.log(`hello ${name}`); //Latest method->mostly used in backend 
+console.log(name[0]);
+//.toString() -> function to convert number to string
+//.toFixed(1)->set precision as 1
