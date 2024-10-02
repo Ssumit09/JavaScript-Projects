8cdbb00d3b0b6a5989f527cd5fbf2af45e937e21
Sumit Kumar Jha <137000536+Ssumit09@users.noreply.github.com>
2024-10-02 12:40:27 +0000

    Update Arrow function

---
+++ JavaScript_code/arrow_function.js
@@ -0,0 +1,42 @@
+// var has global scope
+// this is contextual based 
+// this will be work inside funtion not under the object
+const user={
+    username:"sumit",   //by default this is used
+    price:99,
+
+    welcomeMessage: function(){
+        console.log(`${this.username} , Welcome to website`);
+    }
+}
+// user.welcomeMessage();
+// user.username="John";  //In this line we changed the context 
+// user.welcomeMessage();
+
+
+// This will  lead to undefined as this doesn't understand the context--> for this arrow function wil used
+// const chai=function(){
+//     let username="ssss"
+//     console.log(this.username);
+// }
+
+//This will also lead to undefined from the below code
+//we infer we cannot use this keyword in bothe regular and arrow function
+// const chai=()=>{
+//     let username="ssss"
+//     console.log(this.username);
+// }
+// chai()
+
+// const add=(num1,num2)=>{
+//     return num1+num2;
+// }
+
+// Implicit declaration no neeed to write return 
+// const add=(num1,num2)=> num1+num2;
+// const add=(num1,num2)=> ( num1+num2 );  //mostly used in react
+
+// IIFE (terms Immediately Invoked Function Expression) -> to remove the pollution of Global Scope
+
+ 
+
